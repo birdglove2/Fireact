4f236956b2c83bf86f60e438037cb552049c0c43
Krittamook Aksornchindarat <krittamook@hotmail.com>
2021-07-05 00:00:21 +0700

    get started2

--- server/src/bin/www.js
+++ server/src/bin/www.js
@@ -24,6 +24,7 @@ const normalizePort = val => {
 /**
  * Get port from environment and store in Express.
  */
+
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 /**
@@ -40,16 +41,16 @@ const onError = error => {
   const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
   // handle specific listen errors with friendly messages
   switch (error.code) {
-  case 'EACCES':
-    console.log(`${bind} requires elevated privileges`);
-    process.exit(1);
-    break;
-  case 'EADDRINUSE':
-    console.log(`${bind} is already in use`);
-    process.exit(1);
-    break;
-  default:
-    throw error;
+    case 'EACCES':
+      console.log(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.log(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
   }
 };
 /**
